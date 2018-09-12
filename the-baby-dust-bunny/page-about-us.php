98ed1b77a6bb891979a973a0ae99d69580f49488
wimonsiri <wpakasame@gmail.com>
2018-09-13 01:01:05 +0700

    update 2

---
+++ the-baby-dust-bunny/page-about-us.php
@@ -0,0 +1,21 @@
+<?php
+/* 
+ * Template Name: About Us
+ */
+ 
+get_header();
+
+?>
+<div class="main-content">
+	<section class="segment segment1 space-huge main-banner bg-innerpage-banner">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2">
+					
+				</div>
+			</div>
+		</div>
+	</section>
+</div>
+<?php
+get_footer();
