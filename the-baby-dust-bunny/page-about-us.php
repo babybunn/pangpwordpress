eae5b53f40ac297b6e828a95498a186dc7552c7e
pangpronto <pangp@prontomarketing.com>
2018-09-13 22:09:29 +0700

    page-artists

--- the-baby-dust-bunny/page-about-us.php
+++ the-baby-dust-bunny/page-about-us.php
@@ -10,12 +10,101 @@ get_header();
 	<section class="segment segment1 space-huge main-banner bg-innerpage-banner">
 		<div class="container">
 			<div class="row">
-				<div class="col-md-8 col-md-offset-2">
-					
+				<div class="col-md-8 col-md-offset-2 text-center">
+					<h1 class="page-title">About us</h1>
+					<p class="page-description">Jabuevents is an international corporate entertainment agency <br class="visible-lg">based in Bangkok, Thailand.</p>
 				</div>
 			</div>
 		</div>
 	</section>
+	<section class="segment segment1 space-medium bg-default">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="main-content-overlay">
+						<div class="row-condensed">
+							<div class="col-md-6">
+								
+							</div>
+							<div class="col-md-6">
+								<div class="right-content">
+									<p>Created in 2004 by Vishal Sood, it rapidly became the leading agency in all type of original visual performers such as magicians, illusionists, circus acts and shows. It represent now more than 3000 artists and work with over 7000 clients worldwide from event planner agencies to television channels.</p>
+									<p>Over the past years, Jabuevents worked on various projects: It produced twice the <a href="#">French President Christmas Show</a>, served as artistic consultant for CCTV in China for several years, put together touring shows in India, collaborated with Cirque du Soleil and Dragone productions on unique shows, produced the most important <a href="#">circus festival in France</a> and sent out performers on events in about 79 countries.</p>
+									<p>Jabuevents is now considered to be one of the most important entertainment corporate agency in the world submitting entertainment for parties and weddings. It is always looking forward to discover new talents and to open new markets in the entertainment business. So stay tuned and «Let us Amaze You»!</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="main-content-overlay related-link">
+						<div class="row-condensed">
+							<div class="col-md-4 text-center">
+								<h3 class="font-18 uppercase"><a href="#">More than 500 <br class="visible-lg">artists</a></h3>
+							</div>
+							<div class="col-md-4 text-center">
+								<h3 class="font-18 uppercase"><a href="#">Something about <br class="visible-lg">events</a></h3>
+							</div>
+							<div class="col-md-4 text-center">
+								<h3 class="font-18 uppercase"><a href="#">Something <br class="visible-lg">about Services</a></h3>
+							</div>
+						</div>
+					</div>
+					<div class="main-content-overlay whats-new last">
+						<p class="uppercase margin-0 text-center">What’s new</p>
+					</div>
+
+					<div class="blog-section blog-style-a">
+						<div class="blogfeed-wrapper">
+							<div class="row-condensed">
+								<article class="article-wrapper col-md-3 col-sm-6 col-xs-12">
+									<figure>
+										<a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/img_blog_01_style_a.png" alt="" class="img-responsive"></a>
+									</figure>
+									<div class="artcle-body">
+										<h3><a href="#">Robots Are Taking Over The Entertainment World!</a></h3>
+										<footer><span class="date">19 March 2016</span></footer>
+									</div>
+								</article>
+								<article class="article-wrapper col-md-3 col-sm-6 col-xs-12">
+									<figure>
+										<a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/img_blog_02_style_a.png" alt="" class="img-responsive"></a>
+									</figure>
+									<div class="artcle-body">
+										<h3><a href="#">Corporate Event Destination Guide: The Canary Islands vs. Balearic Islands</a></h3>
+										<footer><span class="date">18 March 2016</span></footer>
+									</div>
+								</article>
+								<article class="article-wrapper col-md-3 col-sm-6 col-xs-12">
+									<figure>
+										<a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/img_blog_03_style_a.png" alt="" class="img-responsive"></a>
+									</figure>
+									<div class="artcle-body">
+										<h3><a href="#">Celebrating 50 Years of The Sound of Music</a></h3>
+										<footer><span class="date">16 March 2016</span></footer>
+									</div>
+								</article>
+								<article class="article-wrapper col-md-3 col-sm-6 col-xs-12">
+									<figure>
+										<a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/img_blog_04_style_a.png" alt="" class="img-responsive"></a>
+									</figure>
+									<div class="artcle-body">
+										<h3><a href="#">French Corporate IPad Magician @ Paris</a></h3>
+										<footer><span class="date">12 March 2016</span></footer>
+									</div>
+								</article>
+							</div>
+							<div class="row-condensed additional-blog">
+								<div class="col-md-12 text-center">
+									<a href="#" class="btn btn-primary btn-transparent">All News</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<div class="bg-gradient section-overlap-arrow"></div>
+	<div class="scroll-up"></div>
 </div>
 <?php
 get_footer();
---
+++ the-baby-dust-bunny/page-artists.php
@@ -0,0 +1,48 @@
+<?php
+/* 
+ * Template Name: Artists
+ */
+
+get_header();
+?>
+
+<div class="main-content">
+	<section class="segment segment1 space-huge main-banner bg_banner-artists">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center">
+					<h1 class="page-title">Artists</h1>
+					<p class="page-description">Either poetry, drama, joy and adrenaline, emotion they convey our <br class="visible-lg">artists make their guests were mindful of your event forever!</p>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="segment segment1 space-medium tab-button-artists">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-3">
+					<h3 class="font-18 uppercase">categories</h3>
+				</div>
+				<div class="col-md-9">
+					<ul class="nav nav-pills button-group-artists">
+						<li role="tablist"><a href="#">Music & Dance</a></li>
+						<li><a href="">Fire & Pyro</a></li>
+						<li><a href="">Cirque</a></li>
+						<li><a href="">Magic</a></li>
+						<li><a href="">Aerial</a></li>
+						<li><a href="">Comedy</a></li>
+						<li><a href="">Artistic</a></li>
+						<li><a href="">Tech Based</a></li>
+						<li><a href="">Ambient & Roving</a></li>
+						<li><a href="">Bollywood</a></li>
+						<li><a href="">Shows</a></li>
+						<li><a href="">One of a Kind</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</section>
+</div>
+
+<?php 
+get_footer();
